9d0e99b8d489f16a517a49a28fad6e3367535c1a
Trey S <fjschmidt292@gmail.com>
2022-05-09 17:17:18 -0400

    getting error that heroku cannot find module still

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const notes = require('./db/db.json');
+const notes = require('./Develop/db/db.json');
 
 
 app.get('/api/notes', (req, res) => {
