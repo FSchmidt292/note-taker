0fe2759608221a31c25dc4482ea8f8549814858e
Trey S <fjschmidt292@gmail.com>
2022-05-09 16:58:47 -0400

    testing GET

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const notes = require('./db/db');
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
